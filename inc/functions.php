78801640200090e7b8591b6149d18a2c0354f4da
Matty Eng <matt.eng.0402@gmail.com>
2018-08-21 21:44:03 -0400

    Did some tinkering with the css. Nothing major. Added an image folder and an image to the header.

--- inc/functions.php
+++ inc/functions.php
@@ -24,7 +24,7 @@ function setlist() {
 		"SELECT cm.song_order, so.song_title 
 		 FROM concert_matrix cm 
 		 JOIN songs so on cm.song_id = so.song_id 
-		 WHERE show_id = 3"
+		 WHERE show_id = 1"
 	 );	
 	} catch (Exception $e) {
 	echo "Unable to retrieve results";
--- index.php
+++ index.php
@@ -1,49 +1,65 @@
+<head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/styles.css">
+	<script>
+		$(document).ready(function(){
+			
+		});
+	</script>
+</head>
+
 <?php
 include("inc/functions.php");
 $wilco_shows = shows();?>
 
-<div>
-  <h2>WILCO SHOWS</h2>
-    <div>
-      <table>
-      <?php
-      foreach($wilco_shows as $concerts){
-        // change sql date_time to Mon 01, Year format
-        $date = $concerts[0];
-		$city = $concerts[1];
-		$venue = $concerts[2];
-        echo "<tr><td>" .
-        $date .
-        "</td><td>" .
-        $city .
-		"</td><td>" .
-        $venue .
-        "</td></tr>";
-      }
-      ?>
-    </table>
-    </div>
-</div>
+<body>
+    <div id="wrapperHeader">
+		<div id="header">
+			<img src="img/wilco_logo.png" alt="logo" />
+		</div>
+	</div>
+	<div>
+	  <h2>WILCO SHOWS</h2>
+		<div>
+		  <table>
+		  <?php
+		  foreach($wilco_shows as $concerts){
+			$date = $concerts[0];
+			$city = $concerts[1];
+			$venue = $concerts[2];
+			echo "<tr><td>" .
+			$date .
+			"</td><td>" .
+			$city .
+			"</td><td>" .
+			$venue .
+			"</td></tr>";
+		  }
+		  ?>
+		</table>
+		</div>
+	</div>
 
-<?php
-$wilco_setlist = setlist();
-?>
+	<?php
+	$wilco_setlist = setlist();
+	?>
 
-<div>
-  <h2>Carbondale Setlist</h2>
-    <div>
-      <table>
-      <?php
-      foreach($wilco_setlist as $songs){
-        $order = $songs[0];
-		$song_title = $songs[1];
-        echo "<tr><td>" .
-        $order .
-        "</td><td>" .
-        $song_title .
-        "</td></tr>";
-      }
-      ?>
-    </table>
-    </div>
-</div>
+	<div>
+	  <h2>Carbondale Setlist</h2>
+		<div>
+		  <table>
+		  <?php
+		  foreach($wilco_setlist as $songs){
+			$order = $songs[0];
+			$song_title = $songs[1];
+			echo "<tr><td>" .
+			$order .
+			"</td><td>" .
+			$song_title .
+			"</td></tr>";
+		  }
+		  ?>
+		</table>
+		</div>
+	</div>
+</body>
